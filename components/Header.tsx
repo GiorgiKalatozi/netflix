c0766377962379755e8e440d2d26455ae61e1e8f
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-11-10 20:21:21 +0400

    header component assets folder hero/icons react icons

---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import smile from "../assets/smile.png";
+import logo from "../assets/logo.svg";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          width={100}
+          height={100}
+          className="cursor-pointer rounded"
+          src={logo}
+          alt="logo"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-link">Home</li>
+          <li className="header-link">TV Shows</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New & Popular</li>
+          <li className="header-link">My List</li>
+        </ul>
+      </div>
+      <div>
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <Image className="cursor-pointer rounded" src={smile} alt="smile" />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Home - Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
+      <Header />
       <main>
         {/* banner */}
         <section>
