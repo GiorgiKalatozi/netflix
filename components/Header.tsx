b41521b67f63044dc27a7478c02a0dbf3aeecf84
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-11-10 20:52:12 +0400

    header ui done

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
           <li className="header-link">My List</li>
         </ul>
       </div>
-      <div>
+      <div className="flex items-center space-x-4 text-sm font-light">
         <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
