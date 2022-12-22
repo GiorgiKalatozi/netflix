4ddaad6dfaf5ce4cc2dc32c7294cc6311c6af52b
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2022-12-22 23:32:30 +0400

    form validation

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,8 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import logo from "../assets/logo.svg";
 import LandingBG from "../assets/landing-bg.webp";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -24,13 +44,47 @@ const Login = () => {
         alt="logo"
       />
 
-      <form>
-        <h1>Sign In</h1>
-        <div>
-          <label>
-            <input type="email" placeholder="Email" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              className="input"
+              type="email"
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email.{" "}
+              </p>
+            )}
           </label>
-          <label></label>
+          <label className="inline-block w-full">
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid password.
+              </p>
+            )}
+          </label>
+        </div>
+        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+          Sign In
+        </button>
+        <div className="text-[gray]">
+          New to Netflix?
+          <button type="submit" className="text-white hover:underline">
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
