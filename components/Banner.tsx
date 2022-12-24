7c566311536317dddbfb4dfe68ae18c5aefdb4c8
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2022-12-24 04:58:41 +0400

    done

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,8 +13,8 @@ interface Props {
 
 const Banner = ({ netflixOriginals }: Props) => {
   const [movie, setMovie] = useState<Movie | null>(null);
-  const [showModal, setShowModal] = useRecoilState(modalState);
   const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  const [showModal, setShowModal] = useRecoilState(modalState);
 
   useEffect(() => {
     setMovie(
@@ -44,7 +44,10 @@ const Banner = ({ netflixOriginals }: Props) => {
         </button>
         <button
           className="banner-btn bg-[gray]/70"
-          onClick={() => setShowModal(true)}
+          onClick={() => {
+            setCurrentMovie(movie);
+            setShowModal(true);
+          }}
         >
           More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
         </button>
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,12 +3,17 @@ import MuiModal from "@mui/material/Modal";
 import { modalState, movieState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { Movie } from "../typings";
+import { Element, Genre } from "../typings";
+import ReactPlayer from "react-player/lazy";
+import { FaPlay, FaVolumeOff, FaVolumeUp } from "react-icons/fa";
+import { HandThumbUpIcon, PlusIcon } from "@heroicons/react/24/solid";
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const [movie, setMovie] = useRecoilState(movieState);
-  const [data, setData] = useState();
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState(false);
 
   useEffect(() => {
     if (!movie) return;
@@ -23,28 +28,99 @@ const Modal = () => {
       )
         .then((response) => response.json())
         .catch((err) => console.log(err.message));
-      setData(data);
-    };
 
-    //  if (data?.videos) {
-    //      const index = data.videos.results.findIndex()
-    //  }
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
+      }
+
+      if (data?.genres) {
+        setGenres(data.genres);
+      }
+    };
 
     fetchMovie();
   }, [movie]);
 
-  console.log(data);
+  console.log(trailer);
 
   const handleClose = () => {
     setShowModal(false);
   };
 
   return (
-    <MuiModal open={showModal} onClose={handleClose}>
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
         <button className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
           <XMarkIcon className="h-6 w-6" />
         </button>
+
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+          />
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                Play
+                <FaPlay className="h-7 w-7 text-black" />
+              </button>
+
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+              <button className="modalButton">
+                <HandThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? <FaVolumeOff /> : <FaVolumeUp />}
+            </button>
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie?.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_data || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(",")}
+                </div>
+                <div>
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.original_language}
+                </div>
+                <div>
+                  <span className="text-[gray]">Total votes: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   );
